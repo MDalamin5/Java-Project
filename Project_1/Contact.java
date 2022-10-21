9df4aa7dd1970561f57bbb5adb51f78d7cb6fc68
MDalamin5 <mdal.amin5@northsouth.edu>
2022-10-22 01:29:18 +0600

    1st day

--- Project_1/Contact.java
+++ Project_1/Contact.java
@@ -1,15 +1,58 @@
 package Project_1;
 
 public class Contact {
+	
+	private String name;
+	private String email;
+	private String phoneNo;
+	
+	
+	public Contact(String name, String phoneNo)
+	{
+		this.name=name;
+		this.phoneNo= phoneNo;
+	}
+	
+	public Contact(String name, String email, String phoneNo)
+	{
+		this.name= name;
+		this.email= email;
+		this.phoneNo= phoneNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
 	public void display()
 	{
-		System.out.println("Here is the Method which is print Nothing");
+		System.out.println("Name: "+name);
+		System.out.println("Email: "+email);
+		System.out.println("Phone Number: "+phoneNo);
+		System.out.println();
 	}
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to Contact list!");
-
-
-	}
-
+	
+	
+	
+	 
 }
---
+++ Project_1/Tester.java
@@ -0,0 +1,14 @@
+ package Project_1;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		
+		Contact ob = new Contact("Alamin","hello@email.com","015425555");
+		
+		ob.display();
+		Contact ob1 = new Contact("Kaysar","01698524552");
+		ob1.display();
+	}
+	
+}
