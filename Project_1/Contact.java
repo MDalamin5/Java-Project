fb35bf6338e05a60d0baf1968d10450d04c1b179
MDalamin5 <mdal.amin5@northsouth.edu>
2022-10-21 23:27:59 +0600

    first commit

---
+++ Project_1/Contact.java
@@ -0,0 +1,15 @@
+package Project_1;
+
+public class Contact {
+	public void display()
+	{
+		System.out.println("Here is the Method which is print Nothing");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Contact list!");
+
+
+	}
+
+}
